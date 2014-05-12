c403d0f17fb4e6ceaad10bef4ed1543a2bf959b6
adamcvikota <adamcvikota@192.168.1.101>
2014-05-12 22:04:49 +0200

    jedalny listok

--- includes/jedalny-listok.php
+++ includes/jedalny-listok.php
@@ -1,6 +1,24 @@
+<?php 
+$pondelok = "";
+$utorok = "";
+$streda = "";
+$stvrtok = "";
+$piatok = "";
+$sobota = "";
+$nedela = "";
+$db = cmsms()->GetDb();
+$weekDaysSelect = $db->GetArray("SELECT prop_name, content FROM cms_content_props where content_id = 57");
+foreach($weekDaysSelect as $weekDay){	
+	if($weekDay["prop_name"] == "content_en"){ $pondelok = $weekDay["content"]; }
+	if($weekDay["prop_name"] == "utorok"){ $utorok = $weekDay["content"]; }
+	if($weekDay["prop_name"] == "streda"){ $streda = $weekDay["content"]; }
+	if($weekDay["prop_name"] == "stvrtok"){ $stvrtok = $weekDay["content"]; }
+	if($weekDay["prop_name"] == "piatok"){ $piatok = $weekDay["content"]; }
+	if($weekDay["prop_name"] == "sobota"){ $sobota = $weekDay["content"]; }
+	if($weekDay["prop_name"] == "nedela"){ $nedela = $weekDay["content"]; }
+}
+?>
 <div class="container content jedalny-listok">
-
-
 <div class="col-md-12">
 <h1><img src="img/titles/jedalny-listok.png" alt="jedálny lístok"></h1>
 </div>
@@ -9,18 +27,15 @@
 <div class="col-md-3 col-md-offset-1 listok"><a class="jedalny" href="#"><img  class="img-responsive" src="img/jedalny-listok/jedalny-listok.png" alt="jedálny lístok"/></a></div>
 <div class="col-md-6 col-md-offset-1">
 <div class="specialita-dna"><div class="inner-content">
-	PONDELOK 6.6.2014
-
-Dnešnej obedovej ponuke sa veru ťažko odoláva, vy nemusíte, teší sa na Vás celý team Pigis na čele so šéfkuchárom Tomášom, ktorý si pre Vás pripravil: 
-
-Kurací vývar s mäsom, zeleninou a rezancami 
-1. Fish and chips s domácou tatarskou omáčkou
-
-2. Domáse smotanové gnocchi so špargľou, paradajkami a mladou cibuľkou
-
-3.Chrumkavý šalát s kuracím mäsom, avokádom a jogurtom 
-
-Na zajtrajší sviatočný deň sme si pre Vás opäť prichystali obľúbenú PIGIS grilovačku. V ponuke budú famózne burgre, šťavnaté mäsko aj rybička.
+<?php 
+echo $pondelok;
+echo $utorok;
+echo $streda;
+echo $stvrtok;
+echo $piatok;
+echo $sobota;
+echo $nedela;
+?>
 </div><!--inner conent-->
 <img  class="img-responsive" src="img/jedalny-listok/chalkboard.png" alt="špecialita dňa"/></div><!--specialita-dna-->
 </div>
