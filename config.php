fae2d05edb62d8a293940b2268fd15c4bebc7830
adamcvikota <adamcvikota@192.168.1.101>
2014-05-13 20:51:53 +0200

    newsletter na frontende

--- config.php
+++ config.php
@@ -5,10 +5,11 @@
 //error_reporting( E_ALL );
 //ini_set('display_errors', 1);
 $config['dbms'] = 'mysql';
-$config['db_hostname'] = 'mariadb55.websupport.sk:3310';
+$config['db_hostname'] = 'mariadb55.websupport.sk';
 $config['db_username'] = 'pigisDev';
 $config['db_password'] = 'pigisDev12*';
 $config['db_name'] = 'pigisDev';
 $config['db_prefix'] = 'cms_';
+$config['db_port'] = 3310;
 $config['timezone'] = 'Europe/Berlin';
 ?>
--- js/main.js
+++ js/main.js
@@ -1,15 +1,47 @@
 $(document).ready(function() {
-			$('#fullpage').fullpage({
-				verticalCentered: false,
-				anchors: ['domov', 'onas', 'jedalny-listok', 'galeria', 'nauc-sa-chefovat', 'kontakt'],
-				'scrollOverflow': true,
-				menu: '#menu',
-				paddingTop: '7%',
-				keyboardScrolling: true,
-				slidesNavigation: true,	
+	$('#fullpage').fullpage({
+		verticalCentered: false,
+		anchors: ['domov', 'onas', 'jedalny-listok', 'galeria', 'nauc-sa-chefovat', 'kontakt'],
+		'scrollOverflow': true,
+		menu: '#menu',
+		paddingTop: '7%',
+		keyboardScrolling: true,
+		slidesNavigation: true,	
+	});
+	$(".newsletterButton").click(function(){
+		if(isValidEmailAddress($(".newsletterEmail").val(),"errorClass")){
+			$.ajax({
+				type: "POST",
+				url: "lib/setMailToNewsletter.php",
+				data: {
+					mail: $(".newsletterEmail").val()
+				},
+				beforeSend: function() 
+				{
+					//sem dat gifko
+				},
+				success: function(response)
+				{
+					console.log(response.status);
+				},
+				error: function(response)
+				{
+					console.log(response.status);
+				}
 			});
-
-
+		}
+	});
+	
+});
+function isValidEmailAddress(emailAddress,errorClass) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    if(pattern.test(emailAddress)){
+    	//$(errorClass).hide();
+    }else{
+    	//$(errorClass).show();
+    }
+    return pattern.test(emailAddress);
+}
 var map;
 function initialize() {        
 var styles = [
@@ -220,5 +252,5 @@ map.mapTypes.set('Styled', styledMapType);}
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
-});
+
 
